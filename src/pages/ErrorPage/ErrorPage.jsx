e4746146cc77ffa73dfba77eeefb3a118e03c35b
HARSHIT BALODI <balodiharshit1907@gmail.com>
2024-04-02 18:58:39 +0530

    updated error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -6,7 +6,6 @@ const ErrorPage = () => {
   return (
     <div className="error-container">
       <h2>path does not found</h2>
-      
       <Button>
         <Link to='/'> Go to Home </Link> 
       </Button>  
